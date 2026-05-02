9c98cb97d03439f6e5afe507a5ae5252837f0a08
manuzala <mzala7722@gmail.com>
2026-05-02 14:17:01 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,8 +1,8 @@
-     import telebot
+import telebot
+import os
 import http.server
 import socketserver
 import threading
-import os
 
 def listen_port():
     port = int(os.environ.get("PORT", 8080))
@@ -10,11 +10,10 @@ def listen_port():
     with socketserver.TCPServer(("", port), handler) as httpd:
         httpd.serve_forever()
 
-threading.Thread(target=listen_port, daemon=True).start()import os
+threading.Thread(target=listen_port, daemon=True).start()
 
 API_TOKEN = os.environ.get('BOT_TOKEN')
 CHANNEL_ID = os.environ.get('CHANNEL_ID')
-
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
@@ -24,9 +23,12 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def forward_to_channel(message):
     try:
-        bot.send_message(CHANNEL_ID, message.text)
-        bot.reply_to(message, "Message channel ma mokli didho chhe!")
+        if CHANNEL_ID:
+            bot.send_message(CHANNEL_ID, message.text)
+            bot.reply_to(message, "Message channel ma share thai gayo!")
+        else:
+            bot.reply_to(message, "Error: CHANNEL_ID set nathi.")
     except Exception as e:
         bot.reply_to(message, f"Error: {e}")
 
-bot.polling()
+bot.infinity_polling()
